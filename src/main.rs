481dd8baf6eb8706353b45b11ede100820bf4c13
Fabien JUIF <fabien.juif@gmail.com>
2023-10-29 14:15:23 +0100

    :sparkles: color hand on button A

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 mod doc_plugin;
 
-use bevy::prelude::*;
+use bevy::{input::gamepad::GamepadButtonChangedEvent, prelude::*};
 use doc_plugin::HelloPlugin;
 
 const JOYSTICK_SCALE: f32 = 200.0;
+const DEFAULT_HAND_COLOR: Color = Color::rgb(0.8, 0.25, 0.24);
 
 #[derive(Component)]
 struct LocalPlayer;
 
 #[derive(Component)]
 struct Name(String);
+
+#[derive(Component)]
+struct Hand;
 
 #[derive(Resource)]
 struct LocalPlayerGamepad(Gamepad);
@@ -18,7 +22,7 @@ fn main() {
     App::new()
         .add_plugins((DefaultPlugins, HelloPlugin))
         .add_systems(Startup, setup)
-        .add_systems(Update, update_axes)
+        .add_systems(Update, (update_axes, update_button_values))
         .run();
 }
 
@@ -41,15 +45,18 @@ fn setup(mut commands: Commands) {
             Name("local_player".to_string()),
         ))
         .with_children(|parent| {
-            parent.spawn(SpriteBundle {
-                sprite: Sprite {
-                    color: Color::rgb(0.8, 0.25, 0.24),
-                    custom_size: Some(Vec2::new(10.0, 10.0)),
+            parent.spawn((
+                SpriteBundle {
+                    sprite: Sprite {
+                        color: DEFAULT_HAND_COLOR,
+                        custom_size: Some(Vec2::new(10.0, 10.0)),
+                        ..default()
+                    },
+                    transform: Transform::from_xyz(0.0, 22.0, 0.1),
                     ..default()
                 },
-                transform: Transform::from_xyz(0.0, 22.0, 0.1),
-                ..default()
-            });
+                Hand {},
+            ));
         });
 }
 
@@ -57,7 +64,7 @@ fn update_axes(
     time: Res<Time>,
     gamepads: Res<Gamepads>,
     axes: Res<Axis<GamepadAxis>>,
-    mut query: Query<&mut Transform, With<LocalPlayer>>,
+    mut query: Query<&mut Transform, (With<LocalPlayer>, With<Children>)>,
 ) {
     // TODO: Affect one gamepad to local player
     for gamepad in gamepads.iter() {
@@ -88,6 +95,28 @@ fn update_axes(
                     Vec3::new(0., 0., 1.),
                     (-left_stick_x).atan2(left_stick_y),
                 );
+            }
+        }
+    }
+}
+
+fn update_button_values(
+    mut events: EventReader<GamepadButtonChangedEvent>,
+    mut parents_query: Query<&Children, With<LocalPlayer>>,
+    mut query: Query<&mut Sprite, With<Hand>>,
+) {
+    for button_event in events.iter() {
+        for children in &mut parents_query {
+            for child in children {
+                if let Ok(mut sprite) = query.get_mut(*child) {
+                    if button_event.button_type == GamepadButtonType::South
+                        && button_event.value != 0.
+                    {
+                        sprite.color = Color::rgb(0.25, 0.75, 0.25)
+                    } else {
+                        sprite.color = DEFAULT_HAND_COLOR
+                    }
+                }
             }
         }
     }
