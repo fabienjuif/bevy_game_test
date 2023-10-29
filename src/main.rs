feeb6f6bfd08f7698c06315bc342c64619f67b61
Fabien JUIF <fabien.juif@gmail.com>
2023-10-29 14:30:26 +0100

    :sparkles: first version of spawner

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ mod doc_plugin;
 
 use bevy::{input::gamepad::GamepadButtonChangedEvent, prelude::*};
 use doc_plugin::HelloPlugin;
+use rand::Rng;
 
-const JOYSTICK_SCALE: f32 = 200.0;
+const JOYSTICK_SCALE: f32 = 200.;
+const MINION_SCALE: f32 = 10.;
 const DEFAULT_HAND_COLOR: Color = Color::rgb(0.8, 0.25, 0.24);
 
 #[derive(Component)]
@@ -15,6 +17,12 @@ struct Name(String);
 #[derive(Component)]
 struct Hand;
 
+#[derive(Component)]
+struct Enemy;
+
+#[derive(Component)]
+struct Minion;
+
 #[derive(Resource)]
 struct LocalPlayerGamepad(Gamepad);
 
@@ -22,7 +30,10 @@ fn main() {
     App::new()
         .add_plugins((DefaultPlugins, HelloPlugin))
         .add_systems(Startup, setup)
-        .add_systems(Update, (update_axes, update_button_values))
+        .add_systems(
+            Update,
+            (update_axes, update_button_values, update_move_minions),
+        )
         .run();
 }
 
@@ -101,23 +112,56 @@ fn update_axes(
 }
 
 fn update_button_values(
+    mut commands: Commands,
     mut events: EventReader<GamepadButtonChangedEvent>,
     mut parents_query: Query<&Children, With<LocalPlayer>>,
     mut query: Query<&mut Sprite, With<Hand>>,
 ) {
     for button_event in events.iter() {
-        for children in &mut parents_query {
-            for child in children {
-                if let Ok(mut sprite) = query.get_mut(*child) {
-                    if button_event.button_type == GamepadButtonType::South
-                        && button_event.value != 0.
-                    {
-                        sprite.color = Color::rgb(0.25, 0.75, 0.25)
-                    } else {
-                        sprite.color = DEFAULT_HAND_COLOR
+        if button_event.button_type == GamepadButtonType::South {
+            for children in &mut parents_query {
+                for child in children {
+                    if let Ok(mut sprite) = query.get_mut(*child) {
+                        if button_event.value != 0. {
+                            sprite.color = Color::rgb(0.25, 0.75, 0.25)
+                        } else {
+                            sprite.color = DEFAULT_HAND_COLOR
+                        }
                     }
                 }
             }
         }
+
+        if button_event.button_type == GamepadButtonType::East && button_event.value != 0. {
+            spawn_enemies(&mut commands)
+        }
+    }
+}
+
+// TODO: Use global tranform?
+fn spawn_enemies(commands: &mut Commands) {
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(1., 0., 0.),
+                custom_size: Some(Vec2::new(50.0, 50.0)),
+                ..default()
+            },
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..default()
+        },
+        Enemy {},
+        Minion {},
+    ));
+}
+
+// TODO: use a seed random maybe Bevy has one
+// TODO: Keep Random value for some frames (need to know how to store data myself)
+fn update_move_minions(time: Res<Time>, mut query: Query<&mut Transform, With<Minion>>) {
+    let mut rng = rand::thread_rng();
+
+    for mut m in &mut query {
+        m.translation.x = time.elapsed_seconds() * MINION_SCALE * rng.gen::<f32>();
+        m.translation.y = time.elapsed_seconds() * MINION_SCALE * rng.gen::<f32>();
     }
 }
